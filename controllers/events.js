64fe5343899123ee783a1382a2202dfcf13e9b50
Luis Enrique Solis Puc <lsolis@grupo-inco.com>
2022-01-12 11:25:18 -0600

    Create and get done

--- controllers/events.js
+++ controllers/events.js
@@ -1,22 +1,42 @@
 const { response } = require('express');
 const Evento = require('../models/Evento');
 
+
 const getEventos = async (req, res = response) => {
-    return res.status(200).json({
-        status: true,
-        msg: 'getEventos'
-    })
+
+    try {
+        const eventos = await Evento.find();
+
+        return res.status(200).json({
+            status: true,
+            eventos: eventos
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
 }
 
 const crearEvento = async (req, res = response) => {
+    const evento = new Evento(req.body);
 
-    //Verificar que tenga el evento
-    console.log(req.body);
+    try {
+        evento.user = req.uid;
+        const eventoGuardado = await evento.save();
+        return res.status(200).json({
+            status: true,
+            evento: eventoGuardado
+        })
 
-    return res.status(200).json({
-        status: true,
-        msg: 'crearEventos'
-    })
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 const actualizarEvento = async (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -2,14 +2,24 @@ const { Router } = require('express');
 const router = Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { check } = require('express-validator');
+const { validar_campos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 //Todas tienen que pasar por la validacion del jwt
 router.use(validarJWT);
+
 //Obtener eventos
 router.get('/', getEventos)
 
 //Crear un nuevo evento
-router.post('/create', crearEvento);
+router.post('/create', [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    validar_campos,
+
+], crearEvento);
 
 //Actualizar un evento
 router.put('/update/:id', actualizarEvento);
